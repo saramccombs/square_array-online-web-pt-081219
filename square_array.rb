dba0977e346e7846e80bb6902b1c956d4b114fb4
Sara McCombs <thesaramccombs@gmail.com>
2019-08-18 19:38:13 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,15 @@
+# method visits each element in the array
+# method squares each element in the array
+# method returns a new array of the squared numbers
+# Don't use any built-in array methods other than .each/.collect / .inject
+
 def square_array(array)
-  # your code here
+  squared_array = []
+  
+  array.each do |number|  
+    squared_array << number**2 
+  end
+  
+  squared_array
 end
+
