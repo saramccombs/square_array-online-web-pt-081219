1757d6c69f8a94df764551d66c65a0ddf944b146
Sara McCombs <thesaramccombs@gmail.com>
2019-08-18 19:25:25 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -6,7 +6,7 @@
 def square_array(array)
   squared_array = []
   
-  array.each |number| {squared_array << number ** 2}
+  array.each |number| { squared_array << number ** 2 }
   
   squared_array
 end
